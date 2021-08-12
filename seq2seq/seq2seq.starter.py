73a4be7fb848fd1950a9099a5fa3430afb33fb3a
huangchao <huangchao.cpp@gmail.com>
2021-08-12 20:20:55 +0800

    update infer

--- seq2seq/seq2seq.starter.py
+++ seq2seq/seq2seq.starter.py
@@ -6,6 +6,7 @@ import os
 import re
 import time
 import json
+import logging
 import argparse
 import unicodedata
 
@@ -25,6 +26,10 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '3'
 table = {ord(f):ord(t) for f,t in zip(
      u'，。！？【】（）％＃＠＆１２３４５６７８９０',
      u',.!?[]()%#@&1234567890')}
+
+logging.basicConfig(filename='run.log', encoding='utf-8', level=logging.DEBUG)
+
+
 
 class NMTDataset(object):
     def __init__(self, file_path):
@@ -206,6 +211,7 @@ class NMT(object):
                  buffer_size=32000, batch_size=64, epochs=5,
                  attention=None, num_examples=None, config_file='nmt.config',
                  is_train=False, train_file=None, is_eval=False, test_file=None,
+                 search_type='beam',
                 ):
 
         self.word_embedding_dim = word_embedding_dim
@@ -220,6 +226,7 @@ class NMT(object):
         self.config_file = config_file
         self.is_train = is_train
         self.train_file = train_file
+        self.search_type = search_type
         if self.is_train:
             self.build_dataset(train_file)
             self.build_model()
@@ -365,7 +372,7 @@ class NMT(object):
             checkpoint_dir = self.checkpoint_dir
         self.checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
-    def decode_beam_search(self, sentences):
+    def decode_beam_search(self, sentences, return_probs=False, return_buffers=False):
 
         sentences = [NMTDataset.preprocess_sentence(line, 'eng') for line in sentences]
 
@@ -395,7 +402,7 @@ class NMT(object):
 #         dec_input = tf.reshape(list(range(1, 1+ self.beam_size * inference_batch_size)),
 #                                [inference_batch_size, self.beam_size, 1]
 #                               )
-        print('dec_input.shape:', dec_input.shape)
+        logging.debug('dec_input.shape:', dec_input.shape)
 
         beam_state_list = [decoder_initial_state] * self.beam_size
         beam_log_probs = tf.zeros((inference_batch_size, self.beam_size, 1))
@@ -405,7 +412,7 @@ class NMT(object):
             [inference_batch_size, self.beam_size, 1])
         # TODO: debug
         # beam_path_list = dec_input
-        print(dec_input.shape, decoder_initial_state[0].shape, decoder_initial_state[1].shape)
+        logging.debug(dec_input.shape, decoder_initial_state[0].shape, decoder_initial_state[1].shape)
         finished_buffer_size = self.beam_size * self.beam_size
         finished_path = np.zeros((inference_batch_size, finished_buffer_size, self.max_length_output), dtype=int)
         finished_probs = np.zeros((inference_batch_size, finished_buffer_size))
@@ -425,22 +432,22 @@ class NMT(object):
                 log_probs += beam_log_probs[:,bid]
                 # topk_indexs = np.argpartition(log_probs, -beam_size)[:,-beam_size:]
                 topk_values, topk_indices = tf.nn.top_k(log_probs, self.beam_size)
-                print("Time {}, iter beam {}".format(t, bid))
-                print('  topk_values', topk_values)
-                print('  topk_indices', topk_indices)
+                logging.debug("Time {}, iter beam {}".format(t, bid))
+                logging.debug('  topk_values', topk_values)
+                logging.debug('  topk_indices', topk_indices)
                 bfs_result.append((state, topk_values, topk_indices))
 
 
 
 
                 for batch_id in range(topk_indices.shape[0]):
-                    print(' input word', self.targ_lang.index_word[inputs[batch_id, 0].numpy()])
+                    logging.debug(' input word', self.targ_lang.index_word[inputs[batch_id, 0].numpy()])
                     for predicted_id in topk_indices[batch_id]:
         #                 if predicted_id not in self.targ_lang.index_word:
         #                     continue
-                        print('\t', predicted_id, self.targ_lang.index_word[predicted_id.numpy()],
+                        logging.debug('\t', predicted_id, self.targ_lang.index_word[predicted_id.numpy()],
                               np.exp(log_probs[batch_id][predicted_id]))
-                    print('-' * 10)
+                    logging.debug('-' * 10)
                 # 对于start只需要search一次。
                 if t == 0:
                     break
@@ -463,10 +470,10 @@ class NMT(object):
             new_path = tf.concat([beam_path_list_expand, tf.expand_dims(beam_pred_ids, -1)], axis=-1)
 
            # find finished result
-            finished_indices = tf.where(beam_pred_ids == nmt.targ_lang.word_index['<end>'])
+            finished_indices = tf.where(beam_pred_ids == self.targ_lang.word_index['<end>'])
 
             if finished_indices.shape[0] > 0:
-                print('handle finised, finished_indices', finished_indices)
+                logging.debug('handle finised, finished_indices', finished_indices)
                 for i in range(finished_indices.shape[0]):
                     batch_id, end_id = finished_indices[i].numpy()
                     if finished_counts[batch_id] >= finished_buffer_size:
@@ -479,13 +486,13 @@ class NMT(object):
                         finished_path[batch_id][finished_counts[batch_id]][j] = cur_finished_path[j]
                     finished_counts[batch_id] += 1
                 # 将已完成的概率降权，使topk无法取到。
-                print('debug', beam_log_probs)
+                logging.debug('debug', beam_log_probs)
                 beam_log_probs = beam_log_probs + \
-                    tf.cast(beam_pred_ids == nmt.targ_lang.word_index['<end>'], tf.float32)\
+                    tf.cast(beam_pred_ids == self.targ_lang.word_index['<end>'], tf.float32)\
                         * tf.reduce_min(beam_log_probs)
 
-                print('finished_probs:', finished_probs)
-                print('finished_path:', finished_path)
+                logging.debug('finished_probs:', finished_probs)
+                logging.debug('finished_path:', finished_path)
 
 
             # get top k gather index
@@ -502,18 +509,18 @@ class NMT(object):
             dec_input = tf.expand_dims(tf.gather_nd(beam_pred_ids, gather_index), -1)
             beam_path_list = tf.gather_nd(new_path, gather_index)
             # print('beam_path_list_expand:', beam_path_list_expand)
-            print('new_path:', beam_path_list)
-            print('beam best translate:')
+            logging.debug('new_path:', beam_path_list)
+            logging.debug('beam best translate:')
             for batch_id in range(beam_path_list.shape[0]):
                 for beam_id in range(beam_path_list.shape[1]):
                     line = ' '.join([self.targ_lang.index_word[predict_id.numpy()] \
                      for predict_id in beam_path_list[batch_id][beam_id]])
-                    print('batch_id {}, beam_id {}, prob {}, {}'.format(
+                    logging.debug('batch_id {}, beam_id {}, prob {}, {}'.format(
                         batch_id, beam_id,
                         np.exp(beam_log_probs[batch_id][beam_id][0].numpy()),
                         line))
             if sum(finished_counts) == len(finished_counts) * finished_buffer_size:
-                print('buffer full filled')
+                logging.debug('buffer full filled')
                 break
         final_probs = []
         final_result = []
@@ -523,43 +530,16 @@ class NMT(object):
                 [self.targ_lang.index_word[wid]\
                     for wid in filter(
                         lambda x: x > 0, finished_path[batch_id][max_index])]))
+        if return_buffers and return_probs:
+            return final_result, final_probs, finished_probs, finished_path
+        elif return_probs:
+            return final_result, final_probs
+        return final_result
 
-        return final_result, final_probs, finished_probs, finished_path
+    def decode_greedy(self, sentences):
+        return [self.decode_greedy_once(line) for line in sentences]
 
-
-            # return bfs_result
-            # beam_path_list = [x[0] for x in bfs_result]
-            # break
-            #dec_input = tf.reshape([predicted_id], [-1,])
-
-#         # 初始概率为0
-#         beam_size = 3
-#         initial_log_probs = tf.constant([[0.] + [-INF] * (beam_size - 1)])
-#         alive_log_probs = tf.tile(initial_log_probs, [inference_batch_size, 1])
-
-#         def _expand_to_beam_size(tensor, beam_size):
-#             """Tiles a given tensor by beam_size.
-#             Args:
-#             tensor: tensor to tile [batch_size, ...]
-#             beam_size: How much to tile the tensor by.
-#             Returns:
-#             Tiled tensor [batch_size, beam_size, ...]
-#             """
-#             tensor = tf.expand_dims(tensor, axis=1)
-#             tile_dims = [1] * tensor.shape.ndims
-#             tile_dims[1] = beam_size
-
-#             return tf.tile(tensor, tile_dims)
-
-#         # Expand each batch and state to beam_size
-#         initial_ids = dec_input
-#         # (inference_batch_size, beam_size)
-#         alive_seq = _expand_to_beam_size(initial_ids, beam_size)
-#         alive_seq = tf.expand_dims(alive_seq, axis=2)  # (batch_size, beam_size, 1)
-
-
-
-    def decode_greedy(self, sentence):
+    def decode_greedy_once(self, sentence):
 
         # Attention plot (to be plotted later on) -- initialized with max_lengths of both target and input
         # attention_plot = np.zeros((max_length_output, max_length_input))
@@ -580,16 +560,16 @@ class NMT(object):
         inputs = tf.convert_to_tensor(inputs)
         inference_batch_size = inputs.shape[0]
 
-        result = '<start> '
+        result = ['<start>']
         enc_start_state = [tf.zeros((inference_batch_size, self.units)),
                            tf.zeros((inference_batch_size, self.units))]
-        print('debug', inputs.shape)
+        logging.debug('debug', inputs.shape)
         enc_out, enc_h, enc_c = self.encoder(inputs, enc_start_state)
 
         decoder_initial_state = [enc_h, enc_c]
         # dec_input: ()
         dec_input = tf.reshape([self.targ_lang.word_index['<start>']], [-1,1])
-        print(dec_input.shape)
+        logging.debug(dec_input.shape)
 
         # Loop until the max_length is reached for the target lang (ENGLISH)
         state = decoder_initial_state
@@ -609,36 +589,48 @@ class NMT(object):
             # print (predicted_id)
 
             # Append the token to the result
-            result += self.targ_lang.index_word[predicted_id] + ' '
+            result.append(self.targ_lang.index_word[predicted_id])
 
             # If <end> token is reached, return the result, input, and attention plot
             if self.targ_lang.index_word[predicted_id] == '<end>':
-                return result
+                break
 
             # The predicted ID is fed back into the model
             dec_input = tf.reshape([predicted_id], [-1,1])
 
-        return result# , sentence# , attention_plot
+        return ' '.join(result)# , sentence# , attention_plot
 
-    def translate(self, sentence):
-        result = self.decode_greedy(sentence)
+    def translate(self, sentences, search_type=None):
+        if search_type is None:
+            search_type = self.search_type
+        if search_type == 'beam':
+            return self.decode_beam_search(sentences)
+        else:
+            return self.decode_greedy(sentences)
         # print(result)
         # result = self.targ_lang.sequences_to_texts(result)
-        print('Input: %s' % (sentence))
-        print('Predicted translation: {}'.format(result))
-        return result
+        # print('Input: %s' % (sentence))
+        # print('Predicted translation: {}'.format(result))
+        # return result
 
     def evaluate_data(self, test_file):
         targets, inputs = NMTDataset.create_dataset(test_file)
-        bleu_score = 0
+        bleu_score = np.array([0.0] * 4)
         targets_len = len(targets)
         for i in range(targets_len):
-            hypothesis = self.translate(inputs[i]).strip().split()[1:-1]
+            input_sentences = [' '.join(inputs[i].strip().split()[1:-1])]
+            print('inputs:', input_sentences)
+            print('Predicted translation: {}'.format(self.translate(input_sentences)))
+            hypothesis = self.translate(input_sentences)[0].strip().split()[1:-1]
             reference = [targets[i].strip().split()[1:-1]]
             print(hypothesis, reference)
-            print(targets[i], inputs[i], self.translate(inputs[i]))
-            bleu_score += sentence_bleu(reference, hypothesis)
-        print("BLEU Score: {}".format(bleu_score / targets_len))
+            print(targets[i], inputs[i],  self.translate(input_sentences)[0])
+            bleu_score[0] += sentence_bleu(reference, hypothesis, weights=(1, 0, 0, 0))
+            bleu_score[1] += sentence_bleu(reference, hypothesis, weights=(0.5, 0.5, 0, 0))
+            bleu_score[2] += sentence_bleu(reference, hypothesis, weights=(0.33, 0.33, 0.33, 0))
+            bleu_score[3] += sentence_bleu(reference, hypothesis, weights=(0.25, 0.25, 0.25, 0.25))
+
+        print("BLEU Score: {}".format(['%.4f' % x for x in (bleu_score / targets_len).tolist()]))
 
 
 
@@ -651,6 +643,8 @@ parser.set_defaults(is_train=False)
 parser.add_argument('--is_eval', dest='is_eval', action='store_true')
 parser.add_argument('--test_file', dest='test_file', default=None)
 parser.set_defaults(is_eval=False)
+
+parser.add_argument('--search_type', dest='search_type', default='beam')
 
 parser.print_help()
 args = parser.parse_args()
@@ -667,7 +661,8 @@ nmt = NMT(checkpoint_dir='./training_attention_checkpoints',
           is_train=args.is_train,
           train_file=args.train_file,
           is_eval=args.is_eval,
-          test_file=args.test_file
+          test_file=args.test_file,
+          search_type=args.search_type
          )
 
 result = nmt.decode_beam_search(["How old are you ?",
@@ -676,6 +671,6 @@ result = nmt.decode_beam_search(["How old are you ?",
                                  "I received your letter.",
                                 ])
 print(result)
-
-nmt.translate("How old are you ?")
+# 
+# nmt.translate("How old are you ?")
 
